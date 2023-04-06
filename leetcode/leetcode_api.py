f69e13e836734fe712242c8c431807ddbb0c03ba
KraykoN <58917010+KraykoN@users.noreply.github.com>
2023-04-06 23:30:13 +0300

    Edit get latest news to use graphql

--- leetcode/leetcode_api.py
+++ leetcode/leetcode_api.py
@@ -112,24 +112,48 @@ def get_daily_problems():
 
 
 def get_latest_news():
-    url = "https://leetcode.com/discuss/"
-    response = requests.get(url)
-    response.raise_for_status()
-    latest_news = []
-    for line in response.text.splitlines():
-        if 'href="/discuss/announcement"' in line:
-            break
-        if 'href="/discuss/' in line:
-            start_index = line.index('href="/discuss/') + len('href="')
-            end_index = line.index('"', start_index)
-            discussion_url = "https://leetcode.com" + line[start_index:end_index]
-            start_index = line.index(">", end_index) + 1
-            end_index = line.index("</a>", start_index)
-            discussion_title = line[start_index:end_index].strip()
-            latest_news.append({"title": discussion_title, "url": discussion_url})
-        if len(latest_news) == 3:  # return at most 3 news items
-            break
-    return latest_news
+    # url = "https://leetcode.com/discuss/"
+    # response = requests.get(url)
+    # response.raise_for_status()
+    # latest_news = []
+    # for line in response.text.splitlines():
+    #     if 'href="/discuss/announcement"' in line:
+    #         break
+    #     if 'href="/discuss/' in line:
+    #         start_index = line.index('href="/discuss/') + len('href="')
+    #         end_index = line.index('"', start_index)
+    #         discussion_url = "https://leetcode.com" + line[start_index:end_index]
+    #         start_index = line.index(">", end_index) + 1
+    #         end_index = line.index("</a>", start_index)
+    #         discussion_title = line[start_index:end_index].strip()
+    #         latest_news.append({"title": discussion_title, "url": discussion_url})
+    #     if len(latest_news) == 3:  # return at most 3 news items
+    #         break
+    query = gql(
+        """
+        query {
+            userRecentTopics(count: 3) { "userRecentTopics", "contestTopics", "allTopics" or "questionTopics
+                ... on TopicNode {
+                    title
+                    url
+                }
+            }
+        }
+    """
+    )
+
+    transport = RequestsHTTPTransport(url="https://leetcode.com/graphql")
+    client = Client(transport=transport, fetch_schema_from_transport=False)
+    result = client.execute(query)
+
+    news_list = []
+
+    for news in result["data"]["userRecentTopics"]:
+        news_title = news["title"]
+        news_url = news["url"]
+        news_list.append((news_title, news_url))
+
+    return news_list
 
 
 if __name__ == "__main__":
@@ -155,3 +179,5 @@ if __name__ == "__main__":
         for item in raw_data["matchedUser"]["submitStats"]["acSubmissionNum"]
     }
     print(raw_data["matchedUser"]["username"] + " " + str(difficulty_counts))
+
+    print(get_latest_news())
