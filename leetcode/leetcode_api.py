ebc9b9720deb50bac4287066bd6173877d38f620
KraykoN <58917010+KraykoN@users.noreply.github.com>
2023-04-08 23:57:40 +0300

    Some new vibes

--- leetcode/leetcode_api.py
+++ leetcode/leetcode_api.py
@@ -64,78 +64,61 @@ def get_new_problems(top_n_problems, paid_only, level_num):
     return new_problems
 
 
-def get_latest_news():
-    # url = "https://leetcode.com/discuss/"
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # latest_news = []
-    # for line in response.text.splitlines():
-    #     if 'href="/discuss/announcement"' in line:
-    #         break
-    #     if 'href="/discuss/' in line:
-    #         start_index = line.index('href="/discuss/') + len('href="')
-    #         end_index = line.index('"', start_index)
-    #         discussion_url = "https://leetcode.com" + line[start_index:end_index]
-    #         start_index = line.index(">", end_index) + 1
-    #         end_index = line.index("</a>", start_index)
-    #         discussion_title = line[start_index:end_index].strip()
-    #         latest_news.append({"title": discussion_title, "url": discussion_url})
-    #     if len(latest_news) == 3:  # return at most 3 news items
-    #         break
-    query = gql(
-        """
-        query {
-            userRecentTopics(count: 3) { "userRecentTopics", "contestTopics", "allTopics" or "questionTopics
-                ... on TopicNode {
-                    title
-                    url
-                }
-            }
-        }
-    """
-    )
+def get_latest_news(theme, num_top_news):
+    url = f"https://leetcode.com/discuss/{theme}"
+    response = requests.get(url)
+    response.raise_for_status()
+    latest_news = []
+    for line in response.text.splitlines():
+        if 'href="/discuss/announcement"' in line:
+            break
+        if 'href="/discuss/' in line:
+            start_index = line.index('href="/discuss/') + len('href="')
+            end_index = line.index('"', start_index)
+            discussion_url = "https://leetcode.com" + line[start_index:end_index]
+            start_index = line.index(">", end_index) + 1
+            end_index = line.index("</a>", start_index)
+            discussion_title = line[start_index:end_index].strip()
+            latest_news.append({"title": discussion_title, "url": discussion_url})
+        if len(latest_news) == 3:  # return at most 3 news items
+            break
 
-    transport = RequestsHTTPTransport(url="https://leetcode.com/graphql")
-    client = Client(transport=transport, fetch_schema_from_transport=False)
-    result = client.execute(query)
 
-    news_list = []
+# def get_available_contests()
 
-    for news in result["data"]["userRecentTopics"]:
-        news_title = news["title"]
-        news_url = news["url"]
-        news_list.append((news_title, news_url))
+# def get_tasks_of_contset(contest_name)
 
-    return news_list
+# def get_user's_solved_tasks_in_details(username)
 
+# def get_task_in_details(task_num)
 
 if __name__ == "__main__":
-    name = '"krayko13"'
-    raw_data = get_user_progress(leetcode_login=name)
-    # response:
-    #     {'matchedUser': {'username': 'krayko13', 'submitStats': {'acSubmissionNum': [
-    #                 {'difficulty': 'All', 'count': 12, 'submissions': 17
-    #                 },
-    #                 {'difficulty': 'Easy', 'count': 8, 'submissions': 9
-    #                 },
-    #                 {'difficulty': 'Medium', 'count': 4, 'submissions': 8
-    #                 },
-    #                 {'difficulty': 'Hard', 'count': 0, 'submissions': 0
-    #                 }
-    #             ]
-    #         }
-    #     }
+    # name = '"krayko13"'
+    # raw_data = get_user_progress(leetcode_login=name)
+    # # response:
+    # #     {'matchedUser': {'username': 'krayko13', 'submitStats': {'acSubmissionNum': [
+    # #                 {'difficulty': 'All', 'count': 12, 'submissions': 17
+    # #                 },
+    # #                 {'difficulty': 'Easy', 'count': 8, 'submissions': 9
+    # #                 },
+    # #                 {'difficulty': 'Medium', 'count': 4, 'submissions': 8
+    # #                 },
+    # #                 {'difficulty': 'Hard', 'count': 0, 'submissions': 0
+    # #                 }
+    # #             ]
+    # #         }
+    # #     }
+    # # }
+
+    # difficulty_counts = {
+    #     item["difficulty"]: item["count"]
+    #     for item in raw_data["matchedUser"]["submitStats"]["acSubmissionNum"]
     # }
+    # print(
+    #     raw_data["matchedUser"]["username"] + " " + str(difficulty_counts)
+    # )  # Output: krayko13 {'All': 12, 'Easy': 8, 'Medium': 4, 'Hard': 0}
 
-    difficulty_counts = {
-        item["difficulty"]: item["count"]
-        for item in raw_data["matchedUser"]["submitStats"]["acSubmissionNum"]
-    }
-    print(
-        raw_data["matchedUser"]["username"] + " " + str(difficulty_counts)
-    )  # Output: krayko13 {'All': 12, 'Easy': 8, 'Medium': 4, 'Hard': 0}
-
-    # print(get_latest_news())
-    problems_lst = get_new_problems(top_n_problems=3, paid_only=False, level_num=1)
-    print(problems_lst)
-    print(len(problems_lst))
+    # problems_lst = get_new_problems(top_n_problems=3, paid_only=False, level_num=1)
+    # print(problems_lst)
+    # print(len(problems_lst))
+    print(get_latest_news())
