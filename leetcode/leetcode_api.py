59a8b7358ad309944eb1d3e15571d1727b10bb04
KraykoN <58917010+KraykoN@users.noreply.github.com>
2023-04-23 01:25:48 +0300

    * feat(leetcode_api.py): add Problem and NewsItem namedtuples to represent LeetCode problems and news items respectively * refactor(leetcode_api.py): refactor get_new_problems function to return a list of Problem objects instead of a list of dictionaries * refactor(leetcode_api.py): refactor get_latest_news function to return a list of NewsItem namedtuples instead of a list of lists

--- leetcode/leetcode_api.py
+++ leetcode/leetcode_api.py
@@ -1,5 +1,6 @@
 import datetime
 from collections import namedtuple
+from typing import List
 
 import gql
 import requests
@@ -16,6 +17,42 @@ class UserProgress(namedtuple("UserProgress", ["username", "difficulty_counts"])
         username (str): The user's LeetCode login.
         difficulty_counts (dict): A dictionary containing the number of problems
             solved at each difficulty level (Easy, Medium, Hard).
+    """
+
+
+class Problem(
+    namedtuple(
+        "Problem",
+        [
+            "question_id",
+            "total_accepted_solutions",
+            "total_submitted_solutions",
+            "title",
+            "level_num",
+            "url",
+            "has_video",
+        ],
+    )
+):
+    # TODO Make docstring
+    """_summary_
+
+    Args:
+        namedtuple (_type_): _description_
+    """
+
+
+class NewsItem(
+    namedtuple(
+        "NewsItem",
+        ["id", "title", "comment_count", "view_count", "pinned", "date", "author"],
+    )
+):
+    # TODO Make docstring
+    """_summary_
+
+    Args:
+        namedtuple (_type_): _description_
     """
 
 
@@ -61,48 +98,80 @@ def get_user_progress(leetcode_login: str) -> UserProgress:
         return UserProgress(result["matchedUser"]["username"], difficulty_counts)
 
 
-def get_new_problems(top_n_problems, paid_only, level_num):
+def get_new_problems(
+    top_n_problems: int, paid_only: bool, level_num: int
+) -> List[Problem]:
+    """
+    This function returns the top n new problems based on paid status and difficulty level.
+
+    Args:
+        top_n_problems (int): The number of new problems to return.
+        paid_only (bool): Whether to include only paid problems.
+        level_num (int): The difficulty level of the problems to include.
+
+    Returns:
+        list: A list of Problem objects.
+    """
+
+    # API endpoint for LeetCode problems
     url = "https://leetcode.com/api/problems/all/"
+
     try:
+        # Send GET request to API endpoint and raise an exception if there's an error
         response = requests.get(url)
-        response.raise_for_status()
     except requests.exceptions.RequestException as e:
+        # If there's an exception, print the error message and return an empty list
         print(f"Error: {e}")
         return []
+
+    # Create an empty list to hold the new problems
     new_problems = []
+
+    # Loop through each problem in the response
     for problem in response.json()["stat_status_pairs"]:
+        # Check if the problem meets the criteria based on paid status and difficulty level
         if (
             problem["paid_only"] is paid_only
             and problem["difficulty"]["level"] is level_num
         ):
+            # If the problem meets the criteria, create a new Problem object and add it to the list
             new_problems.append(
-                {
-                    "question_id": problem["stat"]["question_id"],
-                    "total_accepted_solutions": problem["stat"]["total_acs"],
-                    "total_submitted_solutions": problem["stat"]["total_submitted"],
-                    "title": problem["stat"]["question__title"],
-                    "level_num": problem["difficulty"]["level"],
-                    "url": f"https://leetcode.com/problems/{problem['stat']['question__title_slug']}/",
-                    "has_video": problem["stat"][
-                        "question__article__has_video_solution"
-                    ],
-                }
+                Problem(
+                    question_id=problem["stat"]["question_id"],
+                    total_accepted_solutions=problem["stat"]["total_acs"],
+                    total_submitted_solutions=problem["stat"]["total_submitted"],
+                    title=problem["stat"]["question__title"],
+                    level_num=problem["difficulty"]["level"],
+                    url=f"https://leetcode.com/problems/{problem['stat']['question__title_slug']}/",
+                    has_video=problem["stat"]["question__article__has_video_solution"],
+                )
             )
-        if len(new_problems) == top_n_problems:  # return at most 3 problems
+
+        # Check if the desired number of new problems have been found
+        if len(new_problems) == top_n_problems:
+            # If the desired number have been found, break out of the loop
             break
+
+    # Return the list of new problems
     return new_problems
 
 
-def get_latest_news(theme, num_top_news):
+def get_latest_news(theme: str, num_top_news: int) -> List[NewsItem]:
     """_summary_
 
     Args:
-        theme (string): _description_
-        num_top_news (int): _description_
+        theme (str): A string representing the category of news to retrieve.
+        num_top_news (int): An integer representing the number of top news to retrieve.
 
     Returns:
-        List: _description_
+        list: A list of namedtuples representing the latest news.
     """
+    # TODO Best practice for check datatype in input params
+
+    # if not isinstance(theme, str):
+    #     raise TypeError("theme must be a string")
+    # if not isinstance(num_top_news, int) or num_top_news <= 0:
+    #     raise ValueError("num_top_news must be a positive integer")
 
     query = load_query("leetcode/gql_scripts/get_news.gql")
 
@@ -115,24 +184,34 @@ def get_latest_news(theme, num_top_news):
         "categories": [f"{theme}"],
     }
 
-    transport = RequestsHTTPTransport(url="https://leetcode.com/graphql")
+    GRAPHQL_URL = "https://leetcode.com/graphql"
+    transport = RequestsHTTPTransport(url=GRAPHQL_URL)
     client = Client(transport=transport, fetch_schema_from_transport=False)
-    result = client.execute(query, variable_values=params)
 
-    difficulty_counts = [
-        [
-            int(node_data["id"]),
-            node_data["title"],
-            node_data["commentCount"],
-            node_data["viewCount"],
-            node_data["pinned"],
-            datetime.datetime.fromtimestamp(node_data["post"]["creationDate"]),
-            node_data["post"]["author"]["username"],
-        ]
-        for node_data in (edge["node"] for edge in result["categoryTopicList"]["edges"])
+    try:
+        result = client.execute(query, variable_values=params)
+    except gql.transport.exceptions.TransportQueryError as e:
+        raise RuntimeError(f"Error executing query: {e}") from e
+
+    # TODO  Best Practice for except raise
+    # except GraphQLError as e:
+    #     raise RuntimeError(f"GraphQL error: {e}") from e
+
+    # Convert the list comprehension to use namedtuples and datetime.fromtimestamp()
+    news_list = [
+        NewsItem(
+            id=int(news["id"]),
+            title=news["title"],
+            comment_count=news["commentCount"],
+            view_count=news["viewCount"],
+            pinned=news["pinned"],
+            date=datetime.datetime.fromtimestamp(news["post"]["creationDate"]),
+            author=news["post"]["author"]["username"],
+        )
+        for news in (edge["node"] for edge in result["categoryTopicList"]["edges"])
     ]
 
-    return difficulty_counts
+    return news_list
 
 
 # def get_available_contests(current_date?)
@@ -144,12 +223,18 @@ def get_latest_news(theme, num_top_news):
 # def get_task_in_details(task_num)
 
 if __name__ == "__main__":
-    print(get_user_progress(leetcode_login="krayko13"))
-    print(get_user_progress(leetcode_login="krayko1366"))
-    print(get_user_progress(leetcode_login=""))
+    # print(get_user_progress(leetcode_login="krayko13"))
+    # print(get_user_progress(leetcode_login="krayko1366"))
+    # print(get_user_progress(leetcode_login=""))
     # {'krayko13': {'All': 12, 'Easy': 8, 'Medium': 4, 'Hard': 0}}
 
     # problems_lst = get_new_problems(top_n_problems=3, paid_only=False, level_num=1)
     # print(problems_lst)
     # print(len(problems_lst))
-    # print(get_latest_news(theme="career", num_top_news=2))
+    # print(problems_lst[0].question_id)
+    # print(problems_lst[1].question_id)
+    # print(problems_lst[2].question_id)
+    news = get_latest_news(theme="career", num_top_news=2)
+    print(news)
+    print(news[0].title)
+    print(news[1].title)
